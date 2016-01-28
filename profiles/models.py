8423b3a8cc866b0e1c0e63a5cd7a93669235a194
Kevin Harvey <kharvey@axialhealthcare.com>
2016-01-28 12:17:36 -0600

    Adds Trainer model, FK from UserProfile

--- profiles/models.py
+++ profiles/models.py
@@ -3,10 +3,16 @@ from django.contrib.auth.models import User
 from Common.models import UUIDMixin
 
 
+class Trainer(models.Model):
+    name = models.CharField(max_length=200, help_text='Full name of trainer')
+
+    def __str__(self):
+        return self.name
+
 
 class ClientProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    trainer = models.CharField(max_length=25)
+    trainer = models.ForeignKey(Trainer)
 
     def __str__(self):
         return
